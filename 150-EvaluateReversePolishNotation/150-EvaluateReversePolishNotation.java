15df0a6d7e2073b773827e978dbd74b7bbe9d609
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2025-08-14 11:34:14 +0530

    Create 150-EvaluateReversePolishNotation

---
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
@@ -0,0 +1,43 @@
+// Last updated: 14/08/2025, 11:34:14
+/*class Solution {
+    public int evalRPN(String[] tokens) {
+        
+    }
+}*/
+import java.util.Stack;
+
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(applyOperation(a, b, token));
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+
+    private int applyOperation(int a, int b, String operator) {
+        switch (operator) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b; // Integer division truncates toward zero in Java
+        }
+        throw new IllegalArgumentException("Invalid operator: " + operator);
+    }
+}
