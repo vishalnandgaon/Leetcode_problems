2a3042beff886214ae91c310966ff09e1e32429c
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2025-08-14 11:34:20 +0530

    Create 88-MergeSortedArray

---
+++ 88-MergeSortedArray/88-MergeSortedArray.java
@@ -0,0 +1,10 @@
+// Last updated: 14/08/2025, 11:34:20
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i=0;i<n;i++){
+            nums1[m+i]=nums2[i];
+        }
+        Arrays.sort(nums1);
+        
+    }
+}
