e42904d9743f775edfd55804b9c4d0620edc7735
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2025-08-14 11:34:28 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.java
@@ -0,0 +1,9 @@
+// Last updated: 14/08/2025, 11:34:28
+class Solution {
+    public int mySqrt(int x) {
+        double ans=Math.sqrt(x);
+        int res= (int) (ans/1);
+        return res;
+        
+    }
+}
