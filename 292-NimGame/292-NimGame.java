beb0589f7e297006d55d601b7fa6c3e12dc3a309
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2025-09-07 12:30:00 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,9 @@
+// Last updated: 07/09/2025, 12:29:59
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0){
+            return false;
+        }  
+        return true; 
+    }
+}
