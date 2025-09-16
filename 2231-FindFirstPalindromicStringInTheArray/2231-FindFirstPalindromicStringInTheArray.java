f84ac63cb14ee0c73d0b3798728451f70a45d244
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2025-09-16 20:03:50 +0530

    Create 2231-FindFirstPalindromicStringInTheArray

---
+++ 2231-FindFirstPalindromicStringInTheArray/2231-FindFirstPalindromicStringInTheArray.java
@@ -0,0 +1,23 @@
+// Last updated: 16/09/2025, 20:03:49
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for (String word : words) {
+            if (isPalindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+
+    private boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
