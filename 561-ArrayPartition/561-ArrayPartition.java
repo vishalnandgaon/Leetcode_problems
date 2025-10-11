ae2045f89bffcbd27059ea793f5a1a027fcd1d94
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2025-10-11 23:35:26 +0530

    Create 561-ArrayPartition

---
+++ 561-ArrayPartition/561-ArrayPartition.java
@@ -0,0 +1,11 @@
+// Last updated: 11/10/2025, 23:35:26
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum=0;
+        for(int i=0;i<nums.length;i+=2){
+            sum+=nums[i];
+        }
+        return sum;
+    }
+}
