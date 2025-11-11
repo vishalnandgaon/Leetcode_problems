ab467e5937a28d3b3ed4691d65ab4e010e43512c
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2025-11-11 21:57:37 +0530

    Create 3834-MinimumOperationsToConvertAllElementsToZero

---
+++ 3834-MinimumOperationsToConvertAllElementsToZero/3834-MinimumOperationsToConvertAllElementsToZero.java
@@ -0,0 +1,17 @@
+// Last updated: 11/11/2025, 21:57:38
+class Solution {
+    public int minOperations(int[] nums) {
+        var stack = new int[nums.length + 1];
+        var top = 0;
+        var ans = 0;
+        for (var i = 0; i < nums.length; i++) {
+            while (stack[top] > nums[i]) {
+                top--;
+                ans++;
+            }
+            if (stack[top] != nums[i])
+                stack[++top] = nums[i];
+        }
+        return ans + top;
+    }
+}
