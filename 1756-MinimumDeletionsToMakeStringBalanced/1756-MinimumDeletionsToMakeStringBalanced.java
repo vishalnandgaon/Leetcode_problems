bb65962841d1c3db2d5b878602186c33979f5631
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-02-16 23:08:18 +0530

    Create 1756-MinimumDeletionsToMakeStringBalanced

---
+++ 1756-MinimumDeletionsToMakeStringBalanced/1756-MinimumDeletionsToMakeStringBalanced.java
@@ -0,0 +1,19 @@
+// Last updated: 16/02/2026, 23:08:17
+class Solution {
+    public int minimumDeletions(String s) {
+        int n = s.length(), res = n;
+        int a = 0, b = 0;
+
+        for (int i = 0; i < n; i++)
+            a += s.charAt(i) & 1;
+
+        for (int i = 0; i < n; i++) {
+            int c = s.charAt(i);
+            a -= c & 1;
+            res = Math.min(res, a + b);
+            b += (c & 1) ^ 1;
+        }
+
+        return res;
+    }
+}
