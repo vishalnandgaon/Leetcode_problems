8c8b5e0ce48fedfd61b522567be36b560cc493a1
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-02-26 22:30:11 +0530

    Add two-pointer method for max area calculation
    Implement two-pointer approach to calculate max water area.

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -4,6 +4,7 @@ class Solution {
         int left=0;
         int right=height.length-1;
         int max_water=0;
+        
         while(left<right){
             int width=right-left;
             int h=Math.min(height[left],height[right]);
