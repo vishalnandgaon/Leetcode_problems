6a89f6d567bec1cd2306e8e84a0640f73894b6ce
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-03-28 10:27:16 +0530

    Create 3215-MatrixSimilarityAfterCyclicShifts

---
+++ 3215-MatrixSimilarityAfterCyclicShifts/3215-MatrixSimilarityAfterCyclicShifts.java
@@ -0,0 +1,23 @@
+// Last updated: 28/03/2026, 10:27:15
+class Solution {
+    public boolean areSimilar(int[][] mat, int k) {
+        int m=mat.length, n=mat[0].length;
+        k=k%n;
+        if(k==0) return true;
+        int[][] arr=new int[m][n];
+        for(int i=0;i<m;i++){
+            if(i%2==0){
+                for(int j=0;j<n;j++){
+                    arr[i][j]=mat[i][(j+k)%n];
+                }
+            }
+            else{
+                for(int j=0;j<n;j++){
+                    arr[i][(j+k)%n]=mat[i][j];
+                }
+            }
+        }
+
+        return Arrays.deepEquals(arr,mat);
+    }
+}
