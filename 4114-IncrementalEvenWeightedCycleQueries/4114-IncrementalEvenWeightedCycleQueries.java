aa42c65df63f86c440d5fb8d6fb65871e8c350a9
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-03-29 22:45:04 +0530

    Create 4114-IncrementalEvenWeightedCycleQueries

---
+++ 4114-IncrementalEvenWeightedCycleQueries/4114-IncrementalEvenWeightedCycleQueries.java
@@ -0,0 +1,56 @@
+// Last updated: 29/03/2026, 22:45:04
+class Solution {
+    int[] parent;
+    int[] rank;
+    int[] parity;
+    public int numberOfEdgesAdded(int n, int[][] edges) {
+        int[][] arr=edges;
+        parent=new int[n];
+        rank=new int[n];
+        parity=new int[n];
+
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            parity[i]=0;
+        }
+
+        int count=0;
+        for(int e[]:arr){
+            int u=e[0],v=e[1],w=e[2];
+
+            if(union(u,v,w)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    private boolean union(int u,int v,int w){
+        int ru=find(u);
+        int rv=find(v);
+
+        if(ru==rv){
+            return ((parity[u]^parity[v]^w)==0);
+        }
+        if(rank[ru]<rank[rv]){
+            parent[ru]=rv;
+            parity[ru]=parity[u]^parity[v]^w;
+        }
+        else{
+            parent[rv]=ru;
+            parity[rv]=parity[u]^parity[v]^w;
+            if(rank[ru]==rank[rv]) rank[ru]++;
+        }
+        return true;
+    }
+
+    private int find(int x){
+        if(parent[x]!=x){
+            int original=parent[x];
+            parent[x]=find(parent[x]);
+            parity[x]^=parity[original];
+        }
+        return parent[x];
+    }
+}
