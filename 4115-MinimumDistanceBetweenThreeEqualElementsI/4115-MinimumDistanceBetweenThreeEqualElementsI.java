ee8d27ea5614139a47320cb6cda7f107a596bba6
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-04-10 22:22:04 +0530

    Create 4115-MinimumDistanceBetweenThreeEqualElementsI

---
+++ 4115-MinimumDistanceBetweenThreeEqualElementsI/4115-MinimumDistanceBetweenThreeEqualElementsI.java
@@ -0,0 +1,29 @@
+// Last updated: 10/04/2026, 22:22:03
+import java.util.*;
+
+class Solution {
+    public int minimumDistance(int[] nums) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        
+        // Store indices
+        for (int i = 0; i < nums.length; i++) {
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+        
+        int ans = Integer.MAX_VALUE;
+        
+        // Check for each number
+        for (List<Integer> list : map.values()) {
+            if (list.size() >= 3) {
+                for (int i = 0; i <= list.size() - 3; i++) {
+                    int i1 = list.get(i);
+                    int i3 = list.get(i + 2);
+                    
+                    ans = Math.min(ans, 2 * (i3 - i1));
+                }
+            }
+        }
+        
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
