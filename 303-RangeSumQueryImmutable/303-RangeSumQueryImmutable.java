18876f1a70f2c86a61e4734cdb91ffaf1c9afd6d
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-09-09 16:30:42 +0530

    Create 303-RangeSumQueryImmutable

---
+++ 303-RangeSumQueryImmutable/303-RangeSumQueryImmutable.java
@@ -0,0 +1,22 @@
+// Last updated: 09/09/2026, 16:30:42
+class NumArray {
+    int[] prefix;
+    public NumArray(int[] nums) {
+        prefix=new int[nums.length];
+        prefix[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            prefix[i]=nums[i]+prefix[i-1];
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        if(left==0) return prefix[right];
+        return prefix[right]-prefix[left-1];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
