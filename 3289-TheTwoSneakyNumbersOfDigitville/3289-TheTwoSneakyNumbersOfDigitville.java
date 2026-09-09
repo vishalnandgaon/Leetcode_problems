951ff57ec9583dc888d185852c4fc06523507018
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-09-09 16:03:39 +0530

    Create 3289-TheTwoSneakyNumbersOfDigitville

---
+++ 3289-TheTwoSneakyNumbersOfDigitville/3289-TheTwoSneakyNumbersOfDigitville.java
@@ -0,0 +1,23 @@
+// Last updated: 09/09/2026, 16:03:39
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        int[] ans=new int[2];
+        Arrays.fill(ans,-1);
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                if(ans[0] ==-1){
+                    ans[0]=nums[i];
+                }
+                else{
+                    ans[1]=nums[i];
+                }
+            }
+            else{
+                set.add(nums[i]);
+            }
+        }
+        Arrays.sort(ans);
+        return ans;
+    }
+}
