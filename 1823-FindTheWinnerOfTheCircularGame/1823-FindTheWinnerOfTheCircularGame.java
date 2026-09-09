99cc317547d584b8cbc78acad52e6994fe642a17
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-09-09 16:14:34 +0530

    Create 1823-FindTheWinnerOfTheCircularGame

---
+++ 1823-FindTheWinnerOfTheCircularGame/1823-FindTheWinnerOfTheCircularGame.java
@@ -0,0 +1,19 @@
+// Last updated: 09/09/2026, 16:14:34
+class Solution {
+    public int findTheWinner(int n, int k) {
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            list.add(i);
+        }
+
+        int curr_idx=0;
+        while(list.size()>1){
+            int del=(curr_idx+k-1)%list.size();
+            list.remove(del);
+            curr_idx=del;
+
+        }
+        return list.get(0);
+        
+    }
+}
