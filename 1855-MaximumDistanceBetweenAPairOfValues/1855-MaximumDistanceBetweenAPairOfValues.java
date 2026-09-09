f144fc766afe4d27c36843c9e8e91b5517b8cf5c
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-09-09 16:14:02 +0530

    Create 1855-MaximumDistanceBetweenAPairOfValues

---
+++ 1855-MaximumDistanceBetweenAPairOfValues/1855-MaximumDistanceBetweenAPairOfValues.java
@@ -0,0 +1,12 @@
+// Last updated: 09/09/2026, 16:14:02
+class Solution {
+    public int maxDistance(int[] A, int[] B) {
+        int i, j = 1;
+
+        for (i = 0; i < A.length && j < B.length; j++)
+            if (A[i] > B[j])
+                i++;
+
+        return j - i - 1;
+    }
+}
