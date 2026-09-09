77c8f0ccc039aae58d8ff5641e41fd868d17b3e0
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-09-09 15:57:57 +0530

    Create 3678-SmallestAbsentPositiveGreaterThanAverage

---
+++ 3678-SmallestAbsentPositiveGreaterThanAverage/3678-SmallestAbsentPositiveGreaterThanAverage.java
@@ -0,0 +1,21 @@
+// Last updated: 09/09/2026, 15:57:57
+class Solution {
+    public int smallestAbsent(int[] nums) {
+        Set<Integer> has=new HashSet<>();
+        int n=nums.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            has.add(nums[i]);
+            sum+=nums[i];
+        }
+        int res=sum/n;
+        int ans=0;
+        for(int i=1;i<=101;i++){
+            if(res<i && !has.contains(i)){
+                ans=i;
+                break;
+            }
+            }
+    return ans;
+    }
+}
