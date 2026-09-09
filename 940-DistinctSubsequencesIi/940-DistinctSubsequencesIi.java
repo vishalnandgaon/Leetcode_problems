c0013370a2694fe8b08a9d0a115b6b444b97d557
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-09-09 16:23:29 +0530

    Create 940-DistinctSubsequencesIi

---
+++ 940-DistinctSubsequencesIi/940-DistinctSubsequencesIi.java
@@ -0,0 +1,19 @@
+// Last updated: 09/09/2026, 16:23:29
+class Solution {
+    int MOD = 1000000007;
+
+    public int distinctSubseqII(String s) {
+        int tot = 0;
+        int[] dp = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i) - 97;
+            int add = (tot - dp[c] + MOD) % MOD;
+
+            dp[c] = 1 + tot;
+            tot = (dp[c] + add) % MOD;
+        }
+
+        return tot;
+    }
+}
