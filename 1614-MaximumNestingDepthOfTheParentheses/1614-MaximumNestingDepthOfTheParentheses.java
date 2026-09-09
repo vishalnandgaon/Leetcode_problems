419d001e7b62b9bb634db4beee7ded076da59e44
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-09-09 16:16:35 +0530

    Create 1614-MaximumNestingDepthOfTheParentheses

---
+++ 1614-MaximumNestingDepthOfTheParentheses/1614-MaximumNestingDepthOfTheParentheses.java
@@ -0,0 +1,18 @@
+// Last updated: 09/09/2026, 16:16:35
+class Solution {
+    public int maxDepth(String s) {
+        Stack<Character> stack=new Stack<>();
+        int max=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                stack.push(s.charAt(i));
+                max=Math.max(max,stack.size());
+
+            }
+            if(s.charAt(i)==')' && !stack.isEmpty()){
+                stack.pop();
+            }
+        }  
+        return max; 
+    }
+}
