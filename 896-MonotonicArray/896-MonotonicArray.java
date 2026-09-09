67df5d9a79e0c40353c8d6ef62c5cc23e0f39275
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-09-09 16:23:57 +0530

    Create 896-MonotonicArray

---
+++ 896-MonotonicArray/896-MonotonicArray.java
@@ -0,0 +1,18 @@
+// Last updated: 09/09/2026, 16:23:57
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                increasing = false;
+            }
+            if (nums[i] < nums[i + 1]) {
+                decreasing = false;
+            }
+        }
+
+        return increasing || decreasing;
+    }
+}
