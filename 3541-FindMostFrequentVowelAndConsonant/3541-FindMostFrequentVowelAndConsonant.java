406fe2a164ba13d9166217696aec09ae89dc7cf6
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-09-09 16:00:18 +0530

    Create 3541-FindMostFrequentVowelAndConsonant

---
+++ 3541-FindMostFrequentVowelAndConsonant/3541-FindMostFrequentVowelAndConsonant.java
@@ -0,0 +1,32 @@
+// Last updated: 09/09/2026, 16:00:18
+class Solution {
+    public boolean isvowel(char ch){
+        return "aeiou".indexOf(ch)!=-1;
+    }
+    public int maxFreqSum(String s) {
+        int[] arr=new int[26];
+
+        for(char ch:s.toCharArray()){
+            arr[ch-'a']++;
+        }
+        int maxvowel=0;
+        int maxconstant=0;
+        for(int i=0;i<26;i++){
+            int freq=arr[i];
+            if(freq==0) continue;
+            char alpha=(char)(i+'a');
+            if(isvowel(alpha)){
+                if(freq>maxvowel){
+                    maxvowel=freq;
+                }
+            }
+            else{
+                if(freq>maxconstant){
+                    maxconstant=freq;
+                }
+            }
+
+        }
+        return maxvowel+maxconstant;
+    }
+}
