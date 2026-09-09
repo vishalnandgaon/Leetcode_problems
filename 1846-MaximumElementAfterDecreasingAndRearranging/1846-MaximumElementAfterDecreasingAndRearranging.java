ee0211ad77b5089beafadcfce6fea44e507b2e0e
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-09-09 16:14:30 +0530

    Create 1846-MaximumElementAfterDecreasingAndRearranging

---
+++ 1846-MaximumElementAfterDecreasingAndRearranging/1846-MaximumElementAfterDecreasingAndRearranging.java
@@ -0,0 +1,18 @@
+// Last updated: 09/09/2026, 16:14:30
+class Solution {
+    private static final int[] freq = new int[100005];
+    public int maximumElementAfterDecrementingAndRearranging(int[] A) {
+        int n = A.length;
+        for (int x : A)
+            freq[Math.min(x, n)]++;
+
+        int res = 1;
+        for (int i = 2; i <= n; i++)
+            res = Math.min(res + freq[i], i);
+
+        for (int i = 0; i <= n; i++)
+            freq[i] = 0;
+
+        return res;
+    }
+}
