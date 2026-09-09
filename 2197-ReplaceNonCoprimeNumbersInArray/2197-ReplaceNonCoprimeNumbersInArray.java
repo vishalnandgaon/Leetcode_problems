7bcb477809b58df752c79405ff7b4b74abbac8bd
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-09-09 16:10:38 +0530

    Create 2197-ReplaceNonCoprimeNumbersInArray

---
+++ 2197-ReplaceNonCoprimeNumbersInArray/2197-ReplaceNonCoprimeNumbersInArray.java
@@ -0,0 +1,49 @@
+// Last updated: 09/09/2026, 16:10:38
+import java.util.*;
+
+class Solution {
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    private int lcm(int a, int b) {
+        return (int)((long) a * b / gcd(a, b));
+    }
+
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int num : nums) {
+            stack.push(num);
+
+            while (stack.size() >= 2) {
+                int top = stack.pop();
+                int next = stack.pop();
+
+                int currentGcd = gcd(top, next);
+
+                if (currentGcd == 1) {
+                    stack.push(next);
+                    stack.push(top);
+                    break;
+                } else {
+                    int merged = lcm(top, next);
+                    stack.push(merged);
+                }
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            result.add(stack.pop());
+        }
+        Collections.reverse(result);
+
+        return result;
+    }
+}
