db4e4066bb8e126747771cf11cb83ae950016f93
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-09-09 15:57:12 +0530

    Create 3739-CountSubarraysWithMajorityElementIi

---
+++ 3739-CountSubarraysWithMajorityElementIi/3739-CountSubarraysWithMajorityElementIi.java
@@ -0,0 +1,26 @@
+// Last updated: 09/09/2026, 15:57:11
+class Solution {
+
+    public long countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        // represents the occurrence count of prefix sums -n, -(n-1), ..., 0, 1, ..., n, with index offset by n.
+        int[] pre = new int[n * 2 + 1];
+        pre[n] = 1;
+        int cnt = n;
+        long ans = 0;
+        long presum = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == target) {
+                presum += pre[cnt];
+                ++cnt;
+                ++pre[cnt];
+            } else {
+                --cnt;
+                presum -= pre[cnt];
+                ++pre[cnt];
+            }
+            ans += presum;
+        }
+        return ans;
+    }
+}
