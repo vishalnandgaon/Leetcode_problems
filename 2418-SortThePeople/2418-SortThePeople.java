ae07ef11384985a6ad80b0b3600e6a16d79f58dc
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2026-09-09 16:09:09 +0530

    Create 2418-SortThePeople

---
+++ 2418-SortThePeople/2418-SortThePeople.java
@@ -0,0 +1,14 @@
+// Last updated: 09/09/2026, 16:09:09
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        HashMap<Integer,String> map=new HashMap<>();
+        for(int i=0;i<names.length;i++){
+            map.put(heights[i],names[i]);
+        }
+        Arrays.sort(heights);
+        for(int i=0;i<names.length;i++){
+            names[i]=map.get(heights[names.length-1-i]);
+        }
+        return names;
+    }
+}
